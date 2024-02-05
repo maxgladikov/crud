4f05a19a4c4b7db0bc413b3f7837d1b488e5b96f
max <msgladikov@gmail.com>
2024-02-05 11:56:02 +0500

    controller test 5

--- src/main/java/com/gladikov/crud/util/ServletUtil.java
+++ src/main/java/com/gladikov/crud/util/ServletUtil.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,6 +16,7 @@ public class ServletUtil {
 	public static <T> T convertJsonToObject(HttpServletRequest request, Class<T> clazz) {
 		T result=null;
 		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(Feature.AUTO_CLOSE_SOURCE, true);
 		try {
 			result = mapper.readValue(request.getReader(), clazz);
 		} catch (IOException e) {
